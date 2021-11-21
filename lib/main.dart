a868bfd6871e7ecf48ba2d793acfc31ffbcf1945
주딘 <joo6077@gmail.com>
2021-11-21 13:32:01 +0900

    font 적용

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: 'NotoSansKR'),
       home: const Home(),
       debugShowCheckedModeBanner: false,
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,8 +14,59 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: SafeArea(child: Container()),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.only(left: 16),
+          children: [
+            const Text(
+              '현재 상영중',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: _screenWidth - 16,
+              height: 196,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: [
+                  Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          '222397.jpg',
+                          width: 104,
+                          height: 159,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        'Moonlight Movie',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
