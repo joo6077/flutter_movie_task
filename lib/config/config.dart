05300fb7f28533d511a1033f3523baa04f632788
주딘 <joo6077@gmail.com>
2021-11-22 10:02:21 +0900

    api key 제거 최종 완성

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String apiKey = '04959d38722820bec95208db53060316';
+  static const String apiKey = ''; // api key 입력
   static const String baseUrl = 'https://api.themoviedb.org/3/movie';
   static const String baseImageUrl = 'https://image.tmdb.org/t/p/original';
 }
